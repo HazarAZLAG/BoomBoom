dace76ac13e57345d859a607490de03fd088d451
HazarAZLAG <hazarfondor@gmail.com>
2024-02-08 14:10:37 +0100

    first commit

---
+++ users.js
@@ -0,0 +1,34 @@
+    // création d'un tableau pour nos utilisateur // 
+    const userList = [
+        {
+            email:"john@doe.fr",
+            password:"azerty",
+
+            avatar:"http://fakeavatar.com",
+
+            nom:"Doe",
+            prenom:"John",
+            age:26,
+            sexe:"homme",
+            
+            telephone:"06060606",
+            adresse:"blablablablablabla",
+            
+            profil:"Hello, My Name Is: John Doe!",
+            tags:["HIPHOP","POP Finlandaise"]
+             
+        },
+        {
+            email:"jahne@balec.fr",
+            age:26,
+            nom:"balec",
+            prenom:"Jahne",
+            sexe:"femme",
+            telephone:"06060606",
+            password:"azerty",
+            profil:"Hello, My Name Is: Jahne balec!",
+            avatar:"http://fakeavatar.com",
+            adresse:"blablablablablabla",
+            tags: [""]
+        }
+    ]
